cd5f77602cf9c9debe6a8d4c568833aa6b38a9f6
Akile Altin <altinakile10@gmail.com>
2023-03-09 17:03:15 +0300

    Initial commit

---
+++ Queries/demo.sql
@@ -0,0 +1,20 @@
+SELECT * FROM EMPLOYEES ;
+-- reads all column from employees table
+SELECT * FROM  DEPARTMENTS;
+--reads all column from departments table
+SELECT FIRST_NAME from EMPLOYEES ;
+--get only first name from employees table
+
+--displaty city names
+SELECT CITY from LOCATIONS;
+
+--get me first name ,last name and salary
+SELECT FIRST_NAME,LAST_NAME,SALARY from EMPLOYEES ;
+
+--get me streed address and postal code
+SELECT STREET_ADDRESS , POSTAL_CODE FROM LOCATIONS;
+
+SELECT DISTINCT FIRST_NAME from  EMPLOYEES ;
+--remove duplicates
+
+
